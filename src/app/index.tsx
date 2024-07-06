7576d9132fbc554b140aefe8ea0bb5a2852cff69
mk347 <>
2024-07-06 16:08:17 -0400

    Setup tabs

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Redirect } from 'expo-router'
+
+export default function Home() {
+  return (
+    <Redirect href="/(tabs)" />
+  )
+}
+
+const styles = StyleSheet.create({})
