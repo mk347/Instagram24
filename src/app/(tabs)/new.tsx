3b87bcb189cd1b22758ba954aa4646ca7a52069e
mk347 <>
2024-07-07 00:17:41 -0400

    Ending 2:13

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,19 +1,44 @@
 import { Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreatePost() {
     const [caption, setCaption] = useState('');
+    const [image, setImage] = useState<string | null>(null);
+
+    useEffect(() => {
+      if(!image) {
+        pickImage();
+      }
+    }, [image])
+    
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
 
     return (
         <View className='p-3 items-center flex-1'>
             {/* Image Picker */}
             <Image
                 source={{
-                    uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+                    uri: image,
                 }}
                 className='w-52 aspect-[4/3] rounded-lg'
             />
-            <Text onPress={() => console.log('Pressed')} className='text-blue-500 font-semibold m-5'>
+            <Text onPress={pickImage} className='text-blue-500 font-semibold m-5'>
                 Change
             </Text>
 
